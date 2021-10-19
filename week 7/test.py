fdc6e1a7b6ca995afdc5699c11ebc90430fa5094
Sometime <tanapoowapat@gmail.com>
2021-10-20 02:12:59 +0700

    test edit file

--- week 7/test.py
+++ week 7/test.py
@@ -1,5 +1,18 @@
-mylist = [2000,1500,3000,9999]
+class test:
+    def __init__(self):
+        self.new_report = ()
+    
+    def __str__(self):
+        return f'{self}'
+    
+    def addrepot(self, report):
+        self.new_report = report
+        price = report[2]
+        mylist = []      
+        mylist.append(price)
+        print(*mylist)
 
-mylist.append(3000)
 
-print(mylist)
+c1 = test()
+c1.addrepot(('25 May 2017', 'change tires' , '1500'))
+c1.addrepot(('25 May 2017', 'change tires' , '9900'))
--- week 7/workstation.py
+++ week 7/workstation.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 class Car:
         def __init__(self, license, brand, color):
             # c = Car('AA1234', 'Honda',  'White')
@@ -15,19 +18,19 @@ class Car:
                 #sort number of car by string
                 return self.license > rhs.license
         
-        def add_report(self, Date, Discription, Price):
+        def add_report(self, new_report):
                 # เพิ่มประวัติซ่อมบำรุงโดยไม่ต้องคืนค่า
                 # new_report เก็บ tuple ('Date','Discription','price')
                 # exemple(('25 May 2017', 'change tires' , '1500'))
-                # c1.add_report(('25 May 2017', 'change tires' , '1500'))
-                self.Price = Price
+                # c1.add_report(('25 May 2017', 'change tires' , 1500))
 
-                self.report.append(Date)
-                self.report.append(Discription)
-                self.report.append(int(Price))
-                #print(self.report)
+                self.new_report = ()
+                self.report.append(list(new_report))
                 self.new_report = tuple(self.report)
-               
+                
+                #print(self.new_report)
+                #print(self.report)
+                return f'{self.report}'
                 
             
                 
@@ -36,17 +39,15 @@ class Car:
                 # return all pass maintenance price
 
                 total = 0
-                for num in self.report:
-                        if type(num) == int:
-                                total += num
-                return f'all pass maintenance price is {total}'
+               # for num in self.report:
+
+                pass
                 
 
         def max_payment(self):
                 #return all maintenance record (Date, discription, price) that have maximun price
                 #in case there is no maintenance record return empty list.
                 
-                return self.new_report
                 pass
 
 c1 = Car('AB1234', 'Honda',  'White')
@@ -55,8 +56,8 @@ c2 = Car('AA1234', 'Toyota', 'Black')
 print(c1)
 print(c2)
 
-c1.add_report('25 May 2017', 'change tires' , '2000')
-c1.add_report('26 May 2017', 'change break' , '1500')
+c1.add_report(('25 May 2017', 'change tires' , 1500))
+c1.add_report(('25 May 2017', 'change tires' , 2000))
 
 print(c1.total_payment())
-print(c1.max_payment())
+
